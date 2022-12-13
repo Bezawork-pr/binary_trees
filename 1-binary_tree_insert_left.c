8d177ec61bfd7e9932b2eefb2ef892975b7393c9
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-12 23:51:25 -0800

    Delete prints

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,16 +16,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
        
 	if (parent->left != NULL)
 		copynode = parent->left;
-	printf("where 0");
 	newnode = binary_tree_node(copyparent, value);
-	printf("where 1");
 	if (newnode == NULL)
 		return (NULL);
-	printf("Where 2");
 	parent->left = newnode;
-	printf("Where 3");
 	if (copynode)
 		newnode->left = copynode;
-	printf("where 4");
 	return (newnode);
 }
