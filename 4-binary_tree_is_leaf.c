76be80696638db3710ebc4a719d2207dbc50da50
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-13 04:19:15 -0800

    Debug

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left || node->right || node == NULL || (node->left && node->right))
+	if (node->left /*|| node->right || node == NULL*/)
+		return (0);
+	if (node->right)
+		return (0);
+	if (node == NULL)
 		return (0);
 	return (1);
 }
