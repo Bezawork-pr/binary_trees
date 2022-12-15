f32ece51f99e37a90fdefe0541048ab215a4c08d
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-15 00:51:52 -0800

    Find balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,39 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - Write a function that measures the balance factor 
+ * max - find max
+ *
+ * @num1: num
+ *
+ * @num2: num
+ *
+ * Return: max
+ */
+int max(int num1, int num2)
+{
+	if (num1 >= num2)
+		return (num1);
+	else
+		return (num2);
+}
+/**
+ * height - measures the height of a binary tree
+ *
+ * @tree: is a pointer to the root node of the tree to measure the height
+ *
+ * Return: Height, otherwise 0 on failure
+ */
+int height(const binary_tree_t *tree)
+{
+	int leftheight, rightheight;
+
+	if (tree == NULL)
+		return (0);
+	leftheight = height(tree->left);
+	rightheight = height(tree->right);
+	return (max(leftheight, rightheight) + 1);
+}
+/**
+ * binary_tree_balance - Write a function that measures the balance factor
  * of a binary tree
  *
  * @tree: a pointer to the root node
@@ -10,7 +43,10 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int leftheight = height(tree->left);
+	int rightheight = height(tree->right);
+
 	if (tree == NULL)
 		return (0);
-
+	return (leftheight - rightheight);
 }
