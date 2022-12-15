6e47859029a932f9826661a001f7355c751be7de
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-15 03:34:25 -0800

    Implement post-order traversal recursively

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,41 +11,9 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	const binary_tree_t *left = tree, *parent;
-	int flag = 0;
-
 	if (tree == NULL || func == NULL)
 		return;
-	if (tree->right == NULL && tree->left == NULL)
-	{
-		func(tree->n);
-		return;
-	}
-	while (left->left)
-	{
-		left = left->left;
-		flag = 1;
-	}
-	while (flag == 1)
-	{
-		parent = left->parent;
-		if (left->right)
-			func(left->right->n);
-		func(left->n);
-		left = parent;
-		if (parent->parent == NULL)
-			flag = 0;
-	}
-	left = left->right;
-	while (left->left)
-	{
-		left = left->left;
-		flag = 1;
-	}
-	func(left->n);
-	left = left->parent;
-	if (left->right)
-		func(left->right->n);
-	func(left->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
