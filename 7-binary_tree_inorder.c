705863a0ba5543ff5ddf6c4fa46f03537a8beda2
Bezawork-pr <bezaworkalx@gmail.com>
2022-12-15 03:25:33 -0800

    upload recusive solution to implement inorder traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,37 +11,9 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	const binary_tree_t *left = tree;
-
 	if (tree == NULL || func == NULL)
 		return;
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		func(tree->n);
-		return;
-	}
-	while (left->left)
-		left = left->left;
-	func(left->n);
-	left = left->parent;
-	while (left)
-	{
-		func(left->n);
-		if (left->right)
-		{
-			if (left == tree)
-				break;
-			func(left->right->n);
-		}
-		left = left->parent;
-	}
-	if (left->right)
-		left = left->right;
-	while (left->left)
-		left = left->left;
-	func(left->n);
-	left = left->parent;
-	func(left->n);
-	if (left->right)
-		func(left->right->n);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
